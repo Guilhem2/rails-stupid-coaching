d186c12f77361b512354881aeeac41925517f141
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-01-09 20:32:33 +0100

    finished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    @answers = ["Hello", "The time is #{Time.now}"]
+    if @question.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question.include?("Hello")
+      @answer = @answers[0]
+    elsif @question.include?("time")
+      @answer = @answers[1]
+    elsif @question == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    # @answer = @answers.select { |answer| answer.include?(@question) }
+    # return @answer
+  end
+
 end
